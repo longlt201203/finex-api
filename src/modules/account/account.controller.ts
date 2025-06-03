8eedfcb6b974069482122af25dd8d07748234b1a
Le Thanh Long <longlt201203@gmail.com>
2025-06-03 11:46:23 +0700

    feat: skip auth for register

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -2,14 +2,14 @@ import { Body, Controller, Post } from "@nestjs/common";
 import { AccountService } from "./account.service";
 import { CreateAccountRequest } from "./dto";
 import { ApiResponseDto, SwaggerApiResponse } from "@utils";
-// import { SkipAuth } from "@modules/auth";
+import { SkipAuth } from "@modules/auth";
 
 @Controller("account")
 export class AccountController {
 	constructor(private readonly accountService: AccountService) {}
 
 	@Post()
-	// @SkipAuth()
+	@SkipAuth()
 	@SwaggerApiResponse(Object)
 	async createAccount(@Body() dto: CreateAccountRequest) {
 		await this.accountService.createAccount(dto);
