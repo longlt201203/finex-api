93fbd4ce238e92d545312b2b7ba03cf4c8aee9af
Le Thanh Long <longlt201203@gmail.com>
2025-06-05 09:18:15 +0700

    Revert "feat: fix serve static"
    This reverts commit b3bd6ccab45355c912f93f5d10fd2ce234db562d.

--- src/modules/front/front.module.ts
+++ src/modules/front/front.module.ts
@@ -1,32 +1,16 @@
-import { Module, MiddlewareConsumer, RequestMethod } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 import { FrontController } from "./front.controller";
 import { FrontService } from "./front.service";
 import { ServeStaticModule } from "@nestjs/serve-static";
-import { join } from "path";
 
 @Module({
 	controllers: [FrontController],
 	providers: [FrontService],
 	imports: [
 		ServeStaticModule.forRoot({
-			rootPath: join(process.cwd(), "public"),
-			exclude: ["/api*"],
+			rootPath: "public",
+			serveRoot: "/",
 		}),
 	],
 })
-export class FrontModule {
-	configure(consumer: MiddlewareConsumer) {
-		consumer
-			.apply((req, res, next) => {
-				// Skip API routes
-				if (req.url.startsWith("/api")) {
-					return next();
-				}
-
-				// For all other routes, serve the index.html
-				res.sendFile(join(process.cwd(), "public", "index.html"));
-			})
-			.exclude({ path: "api/(.*)", method: RequestMethod.ALL })
-			.forRoutes({ path: "*", method: RequestMethod.ALL });
-	}
-}
+export class FrontModule {}
